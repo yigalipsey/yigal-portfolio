df76e366c00a6f06b4eee718df6bce82f76241dc
yigalipsey <yigalipsey1996@example.com>
2025-01-26 20:44:27 +0700

    ok

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useRef, useState, useEffect } from "react";
+import Script from "next/script";
 
 function useClientEffect(effect, deps) {
   const [isClient, setIsClient] = useState(false);
@@ -177,6 +178,7 @@ const AboutMe = () => {
 
   return (
     <div className="flex relative justify-center mt-10 md:mb-28 mb-10 items-center bg-cover bg-center bg-no-repeat">
+      <Script src="https://cdn.lordicon.com/lordicon.js" />
       <div ref={topTriggerRef} className="h-1 w-full absolute top-0" />
       <div className="w-[90%] dotted-background mx-auto py-4.5 p-4 sm:p-8 relative">
         <div className="bg-black w-full p-4 py-6">
@@ -281,11 +283,26 @@ const AboutMe = () => {
                 </p>
 
                 <div className="flex relative justify-center mt-3">
-                  <svg
+                  <lord-icon
                     ref={bottomSvgRef}
-                    className={`w-12 h-12 absolute left-[5%] bottom-4 text-white rotate-12 fill-current transition-all duration-1000 ease-in-out`}
-                    viewBox="48.0129 48.1783 416 415.6"
+                    src="https://cdn.lordicon.com/kkjdkiwn.json"
+                    trigger={
+                      rocketState === "moving" || rocketState === "returning"
+                        ? "loop"
+                        : ""
+                    }
+                    state={
+                      rocketState === "moving" || rocketState === "returning"
+                        ? "loop-flying"
+                        : ""
+                    }
+                    colors="primary:#ffffff,secondary:#ffffff"
                     style={{
+                      width: "50px",
+                      height: "50px",
+                      position: "absolute",
+                      left: "5%",
+                      bottom: "10px",
                       transform:
                         rocketState !== "onEarth"
                           ? `translate(${rocketPosition.x - 15}px, ${
@@ -296,9 +313,7 @@ const AboutMe = () => {
                           : "none",
                       transition: "all 1s ease-in-out",
                     }}
-                  >
-                    <path d="M461.81,53.81a4.4,4.4,0,0,0-3.3-3.39c-54.38-13.3-180,34.09-248.13,102.17a294.9,294.9,0,0,0-33.09,39.08c-21-1.9-42-.3-59.88,7.5-50.49,22.2-65.18,80.18-69.28,105.07a9,9,0,0,0,9.8,10.4l81.07-8.9a180.29,180.29,0,0,0,1.1,18.3,18.15,18.15,0,0,0,5.3,11.09l31.39,31.39a18.15,18.15,0,0,0,11.1,5.3,179.91,179.91,0,0,0,18.19,1.1l-8.89,81a9,9,0,0,0,10.39,9.79c24.9-4,83-18.69,105.07-69.17,7.8-17.9,9.4-38.79,7.6-59.69a293.91,293.91,0,0,0,39.19-33.09C427.82,233.76,474.91,110.9,461.81,53.81ZM298.66,213.67a42.7,42.7,0,1,1,60.38,0A42.65,42.65,0,0,1,298.66,213.67Z" />
-                  </svg>
+                  ></lord-icon>
                   <button
                     className="bg-black border border-1 mt-3 border-[#F3E618] hover:text-[#F3E618] font-bold py-2 px-4 rounded"
                     onClick={openWhatsApp}
